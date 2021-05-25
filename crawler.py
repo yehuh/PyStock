b394481d611ab2fe64cb12329ef36e494a0e6378
yehuh <yeh_uh@yahoo.com.tw>
2021-05-25 20:49:14 +0800

    remove useless columns of dataframe

--- crawler.py
+++ crawler.py
@@ -122,8 +122,9 @@ print(str(roc_year))
 
 
 #下載股價
-DaysToCalc = 5
-r=[]
+DaysToCalc = 2
+r_counter=[]
+r_market=[]
 
 whaha = 'https://www.tpex.org.tw/web/stock/aftertrading/daily_close_quotes/stk_quote_result.php?l=zh-tw&o=csv&d=' + str(roc_year)
 whaha+='/'
@@ -135,7 +136,7 @@ print(whaha)
 market_stock = []
 market_stock.append(real_work_day[0].max)
 for i in range(DaysToCalc):
-    r.append(requests.post('https://www.tpex.org.tw/web/stock/aftertrading/daily_close_quotes/stk_quote_result.php?l=zh-tw&o=csv&d=' + str(roc_year) + '/' + real_work_day[i].strftime("%m/%d") + '&s=0,asc,0'))
+    r_counter.append(requests.post('https://www.tpex.org.tw/web/stock/aftertrading/daily_close_quotes/stk_quote_result.php?l=zh-tw&o=csv&d=' + str(roc_year) + '/' + real_work_day[i].strftime("%m/%d") + '&s=0,asc,0'))
 	#r.append(requests.post('https://www.twse.com.tw/exchangeReport/MI_INDEX?response=csv&date=' + real_work_day[i].strftime("%Y%m%d") + '&type=ALL'))
 
 
@@ -149,9 +150,9 @@ for i in range(DaysToCalc):
 
 
 couter_stock_data =[]
-df =[]
+df_counter =[]
 for idd in range(DaysToCalc):
-    k = r[idd].text.split("\n")
+    k = r_counter[idd].text.split("\n")
     index_line = 0
     for i in range(len(k)):
         if(k[i].find("代號")!=-1):
@@ -161,8 +162,11 @@ for idd in range(DaysToCalc):
     for i in range(index_line):
         k.pop(0)
     
-    stock_data_index = k[idd].split(",")
-
+    stock_data_index = k[0].split(",")
+    print("                   ")
+    print("dataframe index" + str(idd))
+    print(stock_data_index)
+    print("                   ")
 
     index_str = ""
     for l in stock_data_index:
@@ -182,20 +186,25 @@ for idd in range(DaysToCalc):
         couter_stock_data[-1] +='\n'
 
     couter_stock_data[-1] = couter_stock_data[-1].strip()
-    df.append(pd.read_csv(StringIO(couter_stock_data[-1]),header=0))
+    df_buff = pd.read_csv(StringIO(couter_stock_data[-1]),header=0)
+    df_buff1 = df_buff.drop(columns=["名稱", "收盤", "開盤", "最高", "最低", "成交筆數","最後買價", "最後買量(千股)"])
+    df_buff2= df_buff1.drop(columns=["最後賣價","最後賣量(千股)","發行股數" ,"次日參考價" , "次日漲停價"])
+    df_buff2 = df_buff2.drop(columns=["均價","成交金額(元)","次日跌停價"])#,"次日跌停價\r" 
+    
+    df_counter.append(df_buff2)
 
 
-'''
-df = pd.read_csv(r[0].text, header = None)
+
+#df = pd.read_csv(r[0].text, header = None)
 print("                   ")
-print("counter stock today")
-print(k[0])
-print(k[1])
-print(k[2])
+print("counter stock today colum name")
+print(df_buff2)
+#print(df_buff.columns[1])
+#print(df_buff.columns[2])
 print("                   ")
 print("                   ")
 print("counter stock today above")
-'''
+
 
 #df = pd.read_csv(StringIO(r.text.replace("=", "")), header=["證券代號" in l for l in r.text.split("\n")].index(True)-1)
 
@@ -230,11 +239,11 @@ deal_cnt_per_day = []
 start_pos = 0
 for k in range(DaysToCalc):
     print("len of df")
-    print(len(df[k].index))
+    print(len(df_counter[k].index))
     print("                 ")
     deal_cnt =[]
     stock_no =[]
-    if(df[k]is None):
+    if(df_counter[k]is None):
         print("dataframe on line not exist")
         print("-------------------")
         continue
@@ -242,20 +251,20 @@ for k in range(DaysToCalc):
     for i in range(int(len(counter_stock))):
         if(counter_stock[i] is None):
             continue
-        for j in range(start_pos,len(df[k].index)):  #,
+        for j in range(start_pos,len(df_counter[k].index)):  #,
             #print("-------------")    
             #print("counter no =")
             #buff0 = df[k].iloc[j,:]
             #buff1 = buff0.loc["代號"]
             #print(buff0)
             #print("              ")
-            if(df[k].iloc[j,0] is None):
+            if(df_counter[k].iloc[j,0] is None):
                 print("dataframe not exist")
                 print("-------------------")
                 continue
-            if (counter_stock[i] == df[k].iloc[j,0]):
+            if (counter_stock[i] == df_counter[k].iloc[j,0]):
                 stock_no.append(counter_stock[i])
-                d_c_temp = str(df[k].iloc[j,8]).replace(",", "")
+                d_c_temp = str(df_counter[k].iloc[j,2]).replace(",", "")
                 #print("-----------------------------------")
                 #print(counter_stock[i]+" stock added")
                 #print("deal count = ")
