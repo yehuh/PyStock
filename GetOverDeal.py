aa6f542a2d257f40f4ca3533d9ee3b60f3a3daef
yehuh <yeh_uh@yahoo.com.tw>
2021-11-28 22:50:50 +0800

    GetOverDeal() is done!!

--- GetOverDeal.py
+++ GetOverDeal.py
@@ -11,12 +11,34 @@ def GetOverDeal(deal_cnt_per_day, total_deal_cnt):
     deal_price_over_deal = []
     total_deal_amount =[]
 
+    print("Deal------Price")
+    deal_pr = str(deal_cnt_per_day[0].loc[0,["DEAL_PRICE"]])
+    deal_str = deal_pr.split()
+    print(float(deal_str[1]))
+    
+
+    
+    
+    
+    error_str = "Stock no:"
+    print(int(total_deal_cnt.loc[0,["DEAL_COUNT"]]))
     over_deal_num = 0
     start_pos = 0
     for stock_index in total_deal_cnt.index:
         deal_cnt_sum = int(total_deal_cnt.loc[stock_index,["DEAL_COUNT"]])
         deal_cnt_today = int(deal_cnt_per_day[0].loc[stock_index,["DEAL_COUNT"]])
-        deal_price_today = deal_cnt_per_day[0].loc[stock_index,["DEAL_PRICE"]]
+        deal_price_today_strs = str(deal_cnt_per_day[0].loc[stock_index,["DEAL_PRICE"]]).split()
+        
+        try:
+            deal_price_today = float(deal_price_today_strs[1])
+        except:
+            deal_price_today = 0
+            stock_no = total_deal_cnt.loc[stock_index,["STOCK_NO"]]
+            error_str ="Deal Price of Stock No: "
+            error_str+=str(stock_no)
+            error_str+=" is not exist!!!!!"
+            print(error_str)
+            
         if(deal_cnt_today > (deal_cnt_sum- deal_cnt_today)):
             deal_cnt_over_deal.append(deal_cnt_today)
             stock_no_over_deal.append(deal_cnt_per_day[0].iloc[stock_index,0])
@@ -24,12 +46,14 @@ def GetOverDeal(deal_cnt_per_day, total_deal_cnt):
             deal_amount_str = deal_price_today * deal_cnt_today
             total_deal_amount.append(deal_price_today * deal_cnt_today)
     
+        '''
         print("deal_amount_str")
         try:
             print(deal_amount_str)
             print(type(deal_amount_str))
         except:
             print("total_deal_amount not exist")
+        '''
     #deal_cnt_sum_no = total_deal_cnt.loc[stock_index,["證券代號"]]
     #deal_cnt_today_no = deal_cnt_per_day[0].loc[stock_index,["證券代號"]]
     #deal_cnt_today = 0
@@ -100,7 +124,7 @@ for i in range(7):
 total_deal_cnt = pd.read_json("20211126And7DaysBeforeDealCntSum.json")
 print("Total Deal Count IS:")
 print(total_deal_cnt)
-'''
+
 over_deal_stock = GetOverDeal(deal_cnt_per_day, total_deal_cnt)
 print("OVER DEAL STOCKS")
 print(over_deal_stock)
