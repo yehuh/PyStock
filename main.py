3e213d56fd189ce74edb2809c354becf68cd8220
yehuh <yeh_uh@yahoo.com.tw>
2022-07-21 23:21:39 +0800

    response work day

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ Created on Thu Jul 21 22:08:07 2022
 
 @author: yehuh
 """
+import GetWorkedDay
+
 def hello_world(request):
     """Responds to any HTTP request.
     Args:
@@ -13,10 +15,12 @@ def hello_world(request):
         Response object using
         `make_response <http://flask.pocoo.org/docs/1.0/api/#flask.Flask.make_response>`.
     """
+    real_work_day = GetWorkedDay.GetWorkedDay(5)
+    
     request_json = request.get_json()
     if request.args and 'message' in request.args:
         return request.args.get('message')
     elif request_json and 'message' in request_json:
         return request_json['message']
     else:
-        return f'Haha World!'
+        return str(real_work_day[0])
