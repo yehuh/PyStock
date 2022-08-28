5be07d62a83160ffa90b76816d659236b5c8f190
yehuh <yeh_uh@yahoo.com.tw>
2022-08-28 15:12:39 +0800

    stock data find modulizing

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ Created on Thu Jul 21 22:08:07 2022
 @author: yehuh
 """
 import GetWorkedDay
+import GetStockDataOnline
 
 def hello_world(request):
     """Responds to any HTTP request.
@@ -24,3 +25,54 @@ def hello_world(request):
         return request_json['message']
     else:
         return str(real_work_day[0])
+
+
+from datetime import datetime, timedelta, date, time
+start_time = datetime.now()
+
+count_days = 7
+deal_cnt_per_day = GetStockDataOnline.GetStockData(count_days)
+print("Caculating Days:")
+print(len(deal_cnt_per_day))
+import DealCnt
+
+total_deal_cnt = DealCnt.CalDealCntSumV2(deal_cnt_per_day,False)
+total_deal_cnt.sort_values("STOCK_NO", inplace = True)
+total_deal_cnt.reset_index(drop=True, inplace=True)
+
+import GetOverDeal
+
+OverDealDf = GetOverDeal.GetOverDeal(deal_cnt_per_day[0], total_deal_cnt)
+
+real_work_day = GetWorkedDay.GetWorkedDay(count_days)
+work_day =[]
+for idex in OverDealDf.index:
+    work_day.append(real_work_day[0].date())
+    try:
+        deal_amount =  float(OverDealDf.at[idex, "DEAL_AMOUNT"])
+    except:
+        stock_no = str(OverDealDf.at[idex,"STOCK_NO"])
+        print("Deal Mount of Stock " + stock_no)
+        print("is not Work!!!!")
+        OverDealDf.drop([idex], axis = 0, inplace = True)
+        continue
+
+
+
+OverDealDf["DATE"] = work_day
+
+print("-------------")
+print("over deal stock")
+print(OverDealDf)
+print("-------------")
+print("             ")
+print("DEAL_AMOUNT > 100000000")
+print(OverDealDf[OverDealDf.DEAL_AMOUNT > 1000000000])
+print("-------------")
+print("             ")
+
+print("Computation is Done!!!!")
+end_time = datetime.now()
+print("-------------")
+print("calculating time is")
+print(end_time - start_time)
