4aab328ed70ee153d7acf2fe4fd77f41dbb3af81
yehuh <yeh_uh@yahoo.com.tw>
2022-08-28 18:26:28 +0800

    test data from big query

--- main.py
+++ main.py
@@ -21,11 +21,10 @@ def hello_pubsub(event, context):
     print("Trigger Act")
     pubsub_message = base64.b64decode(event['data']).decode('utf-8')
     count_days = 7
-    real_work_day = GetWorkedDay.GetWorkedDay(count_days)
     start_time = datetime.now()
     
-    #deal_cnt_per_day = GetStockDataOnline.GetStockData(count_days)
-    '''
+    deal_cnt_per_day = GetStockDataOnline.GetStockData(count_days)
+    
     total_deal_cnt = DealCnt.CalDealCntSumV2(deal_cnt_per_day,False)
     total_deal_cnt.sort_values("STOCK_NO", inplace = True)
     total_deal_cnt.reset_index(drop=True, inplace=True)
@@ -39,29 +38,32 @@ def hello_pubsub(event, context):
         try:
             deal_amount =  float(OverDealDf.at[idex, "DEAL_AMOUNT"])
         except:
-            stock_no = str(OverDealDf.at[idex,"STOCK_NO"])
+            #stock_no = str(OverDealDf.at[idex,"STOCK_NO"])
             OverDealDf.drop([idex], axis = 0, inplace = True)
             continue
 
 
 
     OverDealDf["DATE"] = work_day
-    OverDealDf_k_milium = OverDealDf[OverDealDf.DEAL_AMOUNT > 1000000000]
+    #OverDealDf_k_milium = OverDealDf[OverDealDf.DEAL_AMOUNT > 1000000000]
     
     df_from_cloud = ToGoogleCloud.GetDF_FromGCP()
 
+    test_day = date(2022, 8, 12)
     work_day_exist = False
-
-    for idex in OverDealDf.index:
-        if(real_work_day[0].date() ==  OverDealDf.at[idex, "DATE"]):
+    for idex in df_from_cloud.index:
+        if(test_day ==  df_from_cloud.at[idex, "DATE"]):#real_work_day[0].date()
             work_day_exist = True
             print("Work day exist")
             break
-
-    if(work_day_exist == False):
-        ToGoogleCloud.DfToGoogleCloud(OverDealDf)
     
-    '''
+    if(work_day_exist == False):
+        print("Work day Not exist")
+
+    #if(work_day_exist == False):
+    #    ToGoogleCloud.DfToGoogleCloud(OverDealDf)
+    
+
         
     end_time = datetime.now()
     print("calculating time is")
@@ -137,7 +139,7 @@ def hello_pubsub(event, context):
     #    return str(real_work_day[0])
 
 
-'''module test
+'''module test'''
 start_time = datetime.now()
 
 count_days = 7
@@ -155,6 +157,20 @@ total_deal_cnt.reset_index(drop=True, inplace=True)
 OverDealDf = GetOverDeal.GetOverDeal(deal_cnt_per_day[0], total_deal_cnt)
 
 real_work_day = GetWorkedDay.GetWorkedDay(count_days)
+
+df_from_cloud = ToGoogleCloud.GetDF_FromGCP()
+
+    
+work_day_exist = False
+for idex in df_from_cloud.index:
+    if(real_work_day[0].date() ==  df_from_cloud.at[idex, "DATE"]):
+        work_day_exist = True
+        print("Work day exist")
+        break
+    
+if(work_day_exist == False):
+    print("Work day Not exist")
+
 work_day =[]
 for idex in OverDealDf.index:
     work_day.append(real_work_day[0].date())
@@ -186,4 +202,4 @@ end_time = datetime.now()
 print("-------------")
 print("calculating time is")
 print(end_time - start_time)
-module test'''
+'''module test'''
