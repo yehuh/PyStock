2f616ffb67092a553835ccf5f3af06d62cff0d3f
yehuh <yeh_uh@yahoo.com.tw>
2023-01-07 00:00:39 +0800

    2023 worked day modify

--- GetWorkedDay.py
+++ GetWorkedDay.py
@@ -43,9 +43,38 @@ holidays_taiwan_2021.append(date(2022, 6, 3))
 holidays_taiwan_2021.append(date(2022, 9, 9))
 holidays_taiwan_2021.append(date(2022, 10, 10))
 
+holidays_taiwan_2023 = []
+holidays_taiwan_2023.append(date(2022, 9, 9))
+holidays_taiwan_2023.append(date(2022, 10, 10))
+holidays_taiwan_2023.append(date(2023, 1, 2))
+holidays_taiwan_2023.append(date(2023, 1, 20))
+holidays_taiwan_2023.append(date(2023, 1, 23))
+holidays_taiwan_2023.append(date(2023, 1, 24))
+holidays_taiwan_2023.append(date(2023, 1, 25))
+holidays_taiwan_2023.append(date(2023, 1, 26))
+holidays_taiwan_2023.append(date(2023, 1, 27))
+holidays_taiwan_2023.append(date(2023, 2, 27))
+holidays_taiwan_2023.append(date(2023, 2, 28))
+holidays_taiwan_2023.append(date(2023, 4, 3))
+holidays_taiwan_2023.append(date(2023, 4, 4))
+holidays_taiwan_2023.append(date(2023, 4, 5))
+holidays_taiwan_2023.append(date(2023, 5, 1))
+holidays_taiwan_2023.append(date(2023, 6, 22))
+holidays_taiwan_2023.append(date(2023, 6, 23))
+holidays_taiwan_2023.append(date(2023, 9, 29))
+holidays_taiwan_2023.append(date(2023, 10, 9))
+holidays_taiwan_2023.append(date(2023, 10, 10))
+#1/20~29
+
 special_work_day = []
 special_work_day.append(date(2022, 1, 22))
 special_work_day.append(date(2022, 9, 11))
+special_work_day.append(date(2023, 1, 7))
+special_work_day.append(date(2023, 2, 4))
+special_work_day.append(date(2023, 2, 18))
+special_work_day.append(date(2023, 3, 25))
+special_work_day.append(date(2023, 6, 17))
+special_work_day.append(date(2023, 9, 23))
 
 def GetWorkedDay(count_days_from_today):
     days_ago = []
@@ -66,7 +95,7 @@ def GetWorkedDay(count_days_from_today):
     
     for dayy in worked_day:
         was_holiday = False
-        for holiday in holidays_taiwan_2021:
+        for holiday in holidays_taiwan_2023:
             if dayy.date() == holiday:
                 was_holiday = True
                 break
